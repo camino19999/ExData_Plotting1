267b56b4a75a4daabf1d3aff5550b25d07a75d45
camino19999 <86229152+camino19999@users.noreply.github.com>
2021-08-04 16:21:19 +0200

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,26 +1,4 @@
 
-R version 4.1.0 (2021-05-18) -- "Camp Pontanezen"
-Copyright (C) 2021 The R Foundation for Statistical Computing
-Platform: x86_64-apple-darwin17.0 (64-bit)
-
-R is free software and comes with ABSOLUTELY NO WARRANTY.
-You are welcome to redistribute it under certain conditions.
-Type 'license()' or 'licence()' for distribution details.
-
-  Natural language support but running in an English locale
-
-R is a collaborative project with many contributors.
-Type 'contributors()' for more information and
-'citation()' on how to cite R or R packages in publications.
-
-Type 'demo()' for some demos, 'help()' for on-line help, or
-'help.start()' for an HTML browser interface to help.
-Type 'q()' to quit R.
-
-[R.app GUI 1.76 (7976) x86_64-apple-darwin17.0]
-
-[Workspace restored from /Users/camino19999/.RData]
-[History restored from /Users/camino19999/.Rapp.history]
 
 > power <- read.table("household_power_consumption.txt",skip=1,sep=";")
 > names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
